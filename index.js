ca29aa70aad62a086c079763c35d0f580b162910
rindou0120 <raven258460@gmail.com>
2025-03-11 22:38:36 +0800

    首次提交

---
+++ index.js
@@ -0,0 +1,16 @@
+$(window).on("scroll", function() {
+    $(".box").each(function() {
+        let triggerBottom = $(window).height() * 0.8; //觸發點設置
+        let boxTop = $(this).offset().top - $(window).scrollTop();
+
+        if (boxTop < triggerBottom) {
+            $(this).addClass("show");
+        } else {
+            $(this).removeClass("show");
+        }
+    });
+});
+
+$(document).ready(function() {
+    $(window).trigger("scroll"); // 初次執行
+});
