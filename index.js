d41f3f141f4decec2cad8d0587800cd78727ede3
rindou0120 <raven258460@gmail.com>
2025-03-16 20:34:32 +0800

    add clawmachine.exe

--- index.js
+++ index.js
@@ -18,3 +18,8 @@ $(document).ready(function() {
 function toggleMenu() {
     document.querySelector(".nav-links").classList.toggle("active");
 }
+
+function openSmallWindow() {
+    // 開啟小視窗，指定 URL 和視窗屬性
+        window.open('https://1drv.ms/p/s!As0plJOKWki5g23V66gVE9CfM0hH?e=JiICPY', 'width=500,height=400,top=100,left=100');
+    }
